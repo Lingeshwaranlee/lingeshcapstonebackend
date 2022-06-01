c648d5605f36a7ebfc479c2dad3a9da2f7ac46dd
lingeshwaran <rishilee03@gmail.com>
2022-06-01 12:25:34 +0530

    removed

--- helper1.js
+++ helper1.js
@@ -3,3 +3,6 @@ import { client } from "./index.js";
 export async function createUser(data) {
   return await client.db("b30wd").collection("users").insertOne(data);
 }
+export async function getuserByName(username) {
+  return await client.db("b30wd").collection("users").findOne({username:username});
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser } from "../helper1.js";
+import { createUser,getuserByName } from "../helper1.js";
 import bcrypt from 'bcrypt';
 const routes = express.Router()
 //get a leave
@@ -22,6 +22,28 @@ routes.post("/signup", async function (request, response) {
   }
   const result = await createUser(newUser);
   response.send(result);
+});
+routes.post("/login", async function (request, response) {
+  const {username,password} = request.body;
+  //db.user.findOne({ username:"lee"})
+  const userFromDB = await getuserByName(username);
+  console.log(userFromDB);
+  if(!userFromDB){
+    response.status(401).send({message:"invalid credential"})
+  }
+  else{
+
+    const  storedPassword =userFromDB.password;
+    const  isPasswordMatch = await bcrypt.compare(password,storedPassword);
+    console.log("isPasswordMatch",isPasswordMatch)
+    if(isPasswordMatch){
+      response.send({message:'successful login'})
+    }
+    else{
+      response.status(401).send({message:"invalid credential"})
+    }
+  }
+
 });
 
 export const usersRouter = routes;
